7834cffcc4107481b01cfd7389eafed5d01af69a
IronBlood <aplshenyang@gmail.com>
2025-05-12 17:40:25 +0800

    todo-comments

---
+++ lua/plugins/todo-comments.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/todo-comments.nvim",
+  event = "VimEnter",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  config = function()
+    local TodoComment = require("todo-comments")
+    local set = vim.keymap.set
+    set("n", "]t", TodoComment.jump_next, { desc = "Next todo comment" })
+    set("n", "[t", TodoComment.jump_prev, { desc = "Previous todo comment" })
+  end,
+}
