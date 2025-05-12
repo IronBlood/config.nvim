a17e798735f8288005ed4e7157b2a8a126aa0766
IronBlood <aplshenyang@gmail.com>
2025-05-12 17:24:29 +0800

    nvim-tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,21 @@
+return {
+  "kyazdani42/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", -- optional, for file icon
+  },
+  config = function()
+    vim.opt.termguicolors = true
+
+    local api = require("nvim-tree.api")
+    local function opts(desc)
+      return {
+        desc = "nvim-tree: " .. desc,
+        noremap = true,
+        silent = true,
+        nowait = true,
+      }
+    end
+    vim.keymap.set("n", "<C-n>", api.tree.toggle, opts("Toggle nvim tree"))
+    require("nvim-tree").setup({})
+  end,
+}
