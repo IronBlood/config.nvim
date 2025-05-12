14b6a53851d2d2172effb097942130ef0b147140
IronBlood <aplshenyang@gmail.com>
2025-05-12 18:37:30 +0800

    treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,15 @@
+return {
+  -- Highlight, edit, and navigate code
+  "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+  },
+  main = "nvim-treesitter.configs", -- Sets main module to use for opts
+  build = ":TSUpdate",
+  -- `master` is the stable branch, while `main` is a rewrite, WIP and imcompatible version
+  branch = "master",
+  lazy = false,
+  config = function()
+    require("config.treesiter").setup()
+  end,
+}
