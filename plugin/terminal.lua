99af366428def0684df05d0672474b0712a3c4d0
IronBlood <aplshenyang@gmail.com>
2025-05-12 16:11:35 +0800

    migrate plugin terminal

---
+++ plugin/terminal.lua
@@ -0,0 +1,101 @@
+local bottom_terminal_height = 5
+
+local state = {
+  floating = {
+    buf = -1,
+    win = -1,
+  },
+  bottom = {
+    buf = -1,
+    win = -1,
+  },
+}
+
+-- double <ESC> to back to normal mode in a terminal buffer
+vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+  callback = function()
+    vim.opt.number = false
+    vim.opt.relativenumber = false
+    vim.bo.filetype = "terminal"
+  end,
+})
+
+local function create_bottom_window(opts)
+  opts = opts or {}
+
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true)
+  end
+
+  vim.cmd.vnew()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, bottom_terminal_height)
+
+  local win = vim.api.nvim_get_current_win()
+  vim.api.nvim_win_set_buf(win, buf)
+
+  return { buf = buf, win = win }
+end
+
+local toggle_bottom_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.bottom.win) then
+    state.bottom = create_bottom_window({ buf = state.bottom.buf })
+    if vim.bo[state.bottom.buf].buftype ~= "terminal" then
+      vim.cmd.term()
+    end
+  else
+    vim.api.nvim_win_close(state.bottom.win, true)
+    state.bottom.win = -1
+  end
+end
+
+local function create_floating_window(opts)
+  opts = opts or {}
+  local width = opts.width or math.floor(vim.o.columns * 0.8)
+  local height = opts.height or math.floor(vim.o.lines * 0.8)
+
+  local col = math.floor((vim.o.columns - width) / 2)
+  local row = math.floor((vim.o.lines - height) / 2)
+
+  local buf = nil
+  if vim.api.nvim_buf_is_valid(opts.buf) then
+    buf = opts.buf
+  else
+    buf = vim.api.nvim_create_buf(false, true) -- No file, scratch buffer
+  end
+
+  local win_config = {
+    relative = "editor",
+    width = width,
+    height = height,
+    col = col,
+    row = row,
+    style = "minimal",
+    border = "rounded",
+  }
+
+  local win = vim.api.nvim_open_win(buf, true, win_config)
+
+  return { buf = buf, win = win }
+end
+
+local toggle_floating_terminal = function()
+  if not vim.api.nvim_win_is_valid(state.floating.win) then
+    state.floating = create_floating_window({ buf = state.floating.buf })
+    if vim.bo[state.floating.buf].buftype ~= "terminal" then
+      vim.cmd.term()
+    end
+  else
+    vim.api.nvim_win_hide(state.floating.win)
+    state.floating.win = -1
+  end
+end
+
+vim.keymap.set({ "n", "t" }, "<space>tf", toggle_floating_terminal, { desc = "[T]oggle [F]loating terminal" })
+vim.keymap.set({ "n", "t" }, "<space>tb", toggle_bottom_terminal, { desc = "[T]oggle [B]ottom terminal" })
