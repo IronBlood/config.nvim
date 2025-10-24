37b7235ad8d3814d65d98fd73de348216d73fbc2
IronBlood <aplshenyang@gmail.com>
2025-10-24 20:20:54 +0800

    fix: conform setup (ebaf82f)

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,3 +1,21 @@
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autofomat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 -- Autoformat
 return {
   "stevearc/conform.nvim",
@@ -17,9 +35,6 @@ return {
       desc = "[F]ormat buffer",
     },
   },
-  config = function()
-    require("config.conform")
-  end,
   opts = {
     notify_on_error = false,
     format_on_save = function(bufnr)
