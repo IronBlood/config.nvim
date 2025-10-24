fd2874ae8fcdddf71688579be740086f10636b28
IronBlood <aplshenyang@gmail.com>
2025-10-24 22:58:23 +0800

    sidekick: sync 2.x keymap

--- lua/plugins/sidekick.lua
+++ lua/plugins/sidekick.lua
@@ -34,6 +34,25 @@ return {
   },
   keys = {
     {
+      "<tab>",
+      function()
+        -- if there is a next edit, jump to it, otherwise apply it if any
+        if not require("sidekick").nes_jump_or_apply() then
+          return "<Tab>" -- fallback to normal tab
+        end
+      end,
+      expr = true,
+      desc = "Goto/Apply Next Edit Suggestion",
+    },
+    {
+      "<c-.>",
+      function()
+        require("sidekick.cli").toggle()
+      end,
+      desc = "Sidekick Toggle",
+      mode = { "n", "t", "i", "x" },
+    },
+    {
       "<leader>aa",
       function()
         require("sidekick.cli").toggle()
@@ -50,12 +69,26 @@ return {
       desc = "Select CLI",
     },
     {
+      "<leader>ad",
+      function()
+        require("sidekick.cli").close()
+      end,
+      desc = "Detach a CLI Session",
+    },
+    {
       "<leader>at",
       function()
         require("sidekick.cli").send({ msg = "{this}" })
       end,
       mode = { "x", "n" },
       desc = "Send This",
+    },
+    {
+      "<leader>af",
+      function()
+        require("sidekick.cli").send({ msg = "{file}" })
+      end,
+      desc = "Send File",
     },
     {
       "<leader>av",
@@ -72,14 +105,6 @@ return {
       end,
       mode = { "n", "x" },
       desc = "Sidekick Select Prompt",
-    },
-    {
-      "<c-.>",
-      function()
-        require("sidekick.cli").focus()
-      end,
-      mode = { "n", "x", "i", "t" },
-      desc = "Sidekick Switch Focus",
     },
   },
 }
