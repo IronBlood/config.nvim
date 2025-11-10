3072c66a5dd2fcbcac2482d2c6fa36767a96a0e8
IronBlood <aplshenyang@gmail.com>
2025-11-11 00:05:20 +0800

    telescope-fzf-native on Windows

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,7 +11,16 @@ return {
       "nvim-telescope/telescope-fzf-native.nvim",
       -- NOTE: If you are having trouble with this installation,
       --       refer to the README for telescope-fzf-native for more instructions.
-      build = "make",
+      build = function()
+        if vim.fn.has("win32") == 1 then
+          -- make sure CMake, and the Microsoft C++ Build Tools are installed on Windows
+          return 'cmake -S. -Bbuild -G "Visual Studio 17 2022" -A x64'
+            .. " && cmake --build build --config Release --target install"
+        else
+          -- for Linux and Linux-like systems
+          return "make"
+        end
+      end,
       cond = function()
         return vim.fn.executable("make") == 1
       end,
