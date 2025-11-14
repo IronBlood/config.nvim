42193302e7583b060d506d95c923371270750c6f
IronBlood <aplshenyang@gmail.com>
2025-11-14 20:48:57 +0800

    build jsregexp on windows

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -9,6 +9,9 @@ return {
       "L3MON4D3/LuaSnip",
       version = "2.*",
       build = (function()
+        if vim.fn.has("win32") == 1 and vim.fn.executable("mingw32-make") then
+          return 'LUA_LDLIBS="-lluajit-5.1" mingw32-make install_jsregexp'
+        end
         if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
           return
         end
