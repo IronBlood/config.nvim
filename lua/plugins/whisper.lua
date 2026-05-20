42fef3c3a0feeda865a2625e2730a7eb32abe1e3
IronBlood <aplshenyang@gmail.com>
2026-05-20 14:07:34 +0800

    use multiple endpoints

--- lua/plugins/whisper.lua
+++ lua/plugins/whisper.lua
@@ -1,17 +1,27 @@
+local parsed = require("utils.dotenv").parse_plugin_env()
+local WHISPER_AUTO = parsed.WHISPER_AUTO or ""
+local WHISPER_EN = parsed.WHISPER_EN or ""
+
 return {
   "IronBlood/whisper.nvim",
   config = function()
     local whisper = require("whisper")
 
     whisper.setup({
-      endpoint = "http://127.0.0.1:8081/inference",
+      endpoints = {
+        auto = WHISPER_AUTO,
+        en = WHISPER_EN,
+      },
     })
 
     vim.keymap.set("i", "<F8>", function()
-      whisper.toggle()
-    end, { desc = "Whisper toggle recording" })
+      whisper.toggle("en")
+    end, { desc = "Whisper toggle recording (Enlish auto-translated)" })
+    vim.keymap.set("i", "<F9>", function()
+      whisper.toggle("auto")
+    end, { desc = "Whisper toggle recording (native language)" })
     vim.keymap.set("n", "<leader>ww", function()
-      whisper.toggle()
-    end, { desc = "Whisper toggle recording" })
+      whisper.toggle("en")
+    end, { desc = "Whisper toggle recording (English auto-translated)" })
   end,
 }
