dc93bb3621fd0ea1707f89582d9a0f7a71b93fd3
IronBlood <aplshenyang@gmail.com>
2026-05-20 14:09:35 +0800

    update todo config

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -10,12 +10,12 @@ return {
     local set = vim.keymap.set
     set("n", "]t", TodoComment.jump_next, { desc = "Next todo comment" })
     set("n", "[t", TodoComment.jump_prev, { desc = "Previous todo comment" })
+    TodoComment.setup({
+      signs = true,
+      merge_keywords = false,
+      highlight = {
+        multiline = false,
+      },
+    })
   end,
-  opts = {
-    signs = true,
-    merge_keywords = false,
-    highlight = {
-      multiline = false,
-    },
-  },
 }
